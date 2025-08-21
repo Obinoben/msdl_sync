c180bf5ce167033b7ff1ccdaeb8fe7504c5553f2
Ben Souverbie <ben@quadrumane.com>
2025-08-21 16:51:10 -0400

    logger

--- scripts/syncer.py
+++ scripts/syncer.py
@@ -7,14 +7,43 @@ import time
 import os
 import fcntl
 import sys
+import logging
+import logging.handlers
+
+def setup_logger(debug=False):
+    logger = logging.getLogger("sync")
+
+    # Niveau de log
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    # Handler vers syslog
+    syslog_handler = logging.handlers.SysLogHandler(address="/dev/log")
+    formatter = logging.Formatter("%(name)s[%(process)d]: %(levelname)s - %(message)s")
+    syslog_handler.setFormatter(formatter)
+
+    # Handler vers stdout
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
+
+    # Éviter doublons si setup_logger est appelé plusieurs fois
+    if not logger.handlers:
+        logger.addHandler(syslog_handler)
+        logger.addHandler(stream_handler)
+
+    return logger
+
 
 class syncer:
-    def __init__(self, config, verbose, dry_run, bucket, force):
+    def __init__(self, config, verbose, dry_run, bucket, force, logger):
         self.global_exit_code = 0
         self.dry_run = dry_run
         self.verbose = verbose or self.dry_run
         self.bucket = bucket
         self.force = force
+        self.logger = logger
         with open(config, "r") as f:
             self.config = yaml.safe_load(f)
 
@@ -31,7 +60,7 @@ class syncer:
 
         self.jobs = self.config.get("jobs", [])
         if not self.jobs:
-            print("No job listed in configuration - exiting")
+            self.logger.critical("No job listed in configuration - exiting")
             sys.exit(1)
         self.jobs_list()
 
@@ -48,13 +77,17 @@ class syncer:
 
             self.source_cmd, self.source_bucket = self.get_rclone_bucket_command("source")
             self.target_cmd, self.target_bucket = self.get_rclone_bucket_command("target")
-
             if self.create_subfolder:
                 self.target_cmd = f"{self.target_cmd}/{self.source_bucket}/"
 
-            self.last_success_file = os.path.join(self.syncer.log_dir, f"{self.source_bucket}.last_success")
-            self.run = self.is_job_runnable()
             self.log_file = os.path.join(self.syncer.log_dir, f"{self.source_bucket}.log")
+            self.last_success_file = os.path.join(self.syncer.log_dir, f"{self.source_bucket}.last_success")
+
+            self.wanted = self.syncer.bucket == "all" or self.syncer.bucket == self.source_bucket
+            self.fisrt_time = not os.path.exists(self.last_success_file)
+            self.due = self.is_job_due()
+            self.run = self.wanted and (self.syncer.force or self.due)
+
 
         def get_rclone_bucket_command(self, context):
             provider = self.job[context]
@@ -63,23 +96,23 @@ class syncer:
             command = f"{provider_name}:{bucket}"
             return command, bucket
 
-        def is_job_runnable(self):
-            ## Skip this job if not asked for
-            if self.syncer.bucket != "all" and self.syncer.bucket != self.source_bucket:
-                print(f"{self.title}: SKIPPED - Not the wanted bucket ({self.syncer.bucket})")
-                return False
-
-            ## Always run if forced
-            if self.syncer.force:
-                print(f"{self.title}: TO RUN - Forced by argument")
-                return True
+        def is_job_due(self):
+            # ## Skip this job if not asked for
+            # if not self.wanted:
+            #     print(f"{self.title}: SKIPPED - Not the wanted bucket ({self.syncer.bucket})")
+            #     return False
+            #
+            # ## Always run if forced
+            # if self.syncer.force:
+            #     print(f"{self.title}: TO RUN - Forced by argument")
+            #     return True
 
             ## Check age
             now_timestamp = int(time.time())
 
             # First run
-            if not os.path.exists(self.last_success_file):
-                print(f"{self.title}: TO RUN - first execution")
+            if self.fisrt_time:
+                self.syncer.logger.debug(f"{self.title}: TO RUN - first execution")
                 return True
 
             with open(self.last_success_file) as f:
@@ -88,14 +121,14 @@ class syncer:
 
             ## Last run too young - skip this run
             if last_success_age < self.max_age_seconds:
-                print(f"{self.title}: SKIPPED - too recent (last success on "
+                self.syncer.logger.debug(f"{self.title}: SKIPPED - too recent (last success on "
                       f"{time.strftime('%Y-%m-%d at %H:%M:%S', time.localtime(last_success))})")
                 return False
 
             ## Last run too old - need a run
-            print(f"{self.title}: TO RUN - too old (last success on "
+            self.syncer.logger.debug(f"{self.title}: TO RUN - too old (last success on "
                   f"{time.strftime('%Y-%m-%d at %H:%M:%S', time.localtime(last_success))})")
-            return True
+            self.run = True
 
         def get_rclone_command(self):
             self.command = [
@@ -113,16 +146,16 @@ class syncer:
                     fcntl.flock(lf, fcntl.LOCK_EX | fcntl.LOCK_NB)
                     self.state = subprocess.call(self.command)
                 except BlockingIOError:
-                    print(f"{self.title}: Another process is already running for {self.source_bucket}, skipping.")
+                    self.syncer.logger.warning(f"{self.title}: Another process is already running for {self.source_bucket}, skipping.")
                     self.state = 1
 
         def exit_command(self):
             if self.state == 0:
-                print(">>> Success")
+                self.syncer.logger.info(">>> Success")
                 with open(self.last_success_file, "w") as f:
                     f.write(str(time.time()))
             else:
-                print(">>> Failed")
+                self.syncer.logger.critical(">>> Failed")
 
     def jobs_list(self):
         self.job_handlers = list()
@@ -138,11 +171,11 @@ class syncer:
             job_handler.get_rclone_command()
 
             if self.verbose:
-                print(" ".join(job_handler.command))
-            print(f"{job_handler.title}: Running in {job_handler.sync_type} mode")
+                self.logger.debug(" ".join(job_handler.command))
+            self.logger.info(f"{job_handler.title}: Running in {job_handler.sync_type} mode")
 
             if self.dry_run:
-                print(">>> Dry run: skipping execution")
+                self.logger.warning(">>> Dry run: skipping execution")
             else:
                 job_handler.run_command()
 
@@ -151,7 +184,7 @@ class syncer:
 
     def jobs_interractive_choice(self):
         if self.bucket != "all":
-            print(f"Bucket to sync already known ({self.bucket})")
+            self.logger.debug(f"Bucket to sync already known ({self.bucket})")
             return
 
         print("Available jobs:")
@@ -161,6 +194,8 @@ class syncer:
         self.bucket = input("\nEnter bucket name or 'all': ").strip()
         if self.bucket == "":
             self.bucket = "all"
+
+        self.logger.debug(f"Chosen bucket to sync: {self.bucket}")
 
 
 
@@ -192,17 +227,25 @@ def main():
         help="Verbose mode"
     )
     parser.add_argument(
+        "-d", "--debug",
+        action="store_true",
+        help="Display debug"
+    )
+    parser.add_argument(
         "--dry-run",
         action="store_true",
         help="Only display, does not run the sync jobs"
     )
     args = parser.parse_args()
 
+    logger = setup_logger(debug=args.debug)
+
     handler = syncer(config = args.config,
                      verbose = args.verbose,
                      dry_run = args.dry_run,
                      bucket = args.bucket,
-                     force = args.force)
+                     force = args.force,
+                     logger = logger)
     handler.jobs_interractive_choice()
     handler.jobs_loop()
 
