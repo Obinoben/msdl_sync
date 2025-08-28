da122b47ce1e22b8ab639e29c51a284c5e49b60b
Ben Souverbie <ben@quadrumane.com>
2025-08-28 17:58:28 -0400

    v2

--- scripts/manager.sh
+++ scripts/manager.sh
@@ -3,34 +3,33 @@
 export MONDAY_API_KEY=$(cat /etc/quadrumane/monday.key)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 MONDAY_ITEM=$(echo "$m" | jq -r '.monday_item_id')
+MONDAY_BOARD=$(curl -s -X POST "https://api.monday.com/v2" \
+                   -H "Authorization: $MONDAY_API_KEY" \
+                   -H "Content-Type: application/json" \
+                   -d "{\"query\": \"query { items(ids: $MONDAY_ITEM) { board { id } } }\"}" \
+                   | jq -r '.data.items[0].board.id')
 
 update_monday_item () {
-  local item_id="$1"
-  local column_id="$2"
-  local value="$3"
+  local board_id="$1"
+  local item_id="$2"
+  local column_id="$3"
+  local value="$4"
 
-  value=$(echo "$value" | sed 's/"/\\\\\\\"/g')
+  # JSON dans un fichier temporaire
+  QUERY_JSON=$(jq -n --arg item "$item_id" \
+                    --arg board "$board_id" \
+                    --arg column "$column_id" \
+                    --argjson val "$value" \
+                    '{query: "mutation { change_simple_column_value(item_id: \($item), board_id: \($board), column_id: \($column), value: \($val)) { id } }"}')
 
   curl -s -X POST "https://api.monday.com/v2" \
     -H "Authorization: $MONDAY_API_KEY" \
     -H "Content-Type: application/json" \
-    -d "{
-      \"query\": \"mutation {
-        change_simple_column_value(
-          item_id: $item_id,
-          column_id: \\\"$column_id\\\",
-          value: \\\"$value\\\"
-        ) {
-          id
-        }
-      }\"
-    }"
+    -d "$QUERY_JSON"
 }
 
-
-
 echo "Message reçu"
-update_monday_item $MONDAY_ITEM "status" '{"label":"Traitement"}'
+update_monday_item $MONDAY_BOARD $MONDAY_ITEM "status" "Traitement"
 global=0
 for archive in $(echo "$m" | jq -r '.archives' | sed 's/,//g'); do
   echo "Lance la synchronisation de ${archive}"
@@ -40,10 +39,10 @@ done
 
 if [[ $global -eq 0 ]]; then
   echo "Tout a fonctionné"
-  update_monday_item $MONDAY_ITEM "status" '{"label":"Succès"}'
+  update_monday_item $MONDAY_BOARD $MONDAY_ITEM "status" "Succès"
 else
   echo "Erreur de sync"
-  update_monday_item $MONDAY_ITEM "status" '{"label":"Échec"}'
+  update_monday_item $MONDAY_BOARD $MONDAY_ITEM "status" "Échec"
 fi
 
 exit $global
