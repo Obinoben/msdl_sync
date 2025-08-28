b015dc8ac91ae121b92e54861fa8319e99280acd
Ben Souverbie <ben@quadrumane.com>
2025-08-28 15:48:35 -0400

    essay ntfy

--- scripts/consumer.sh
+++ scripts/consumer.sh
@@ -8,6 +8,6 @@ ntfy_queue="msdl_sync_eQgAtgwyvQ85hlum"
 consumer_lock=/tmp/ntfy.lock
 
 # Ne lance qu'un seul consumer
-flock -n ${consumer_lock} -c "ntfy subscribe ${ntfy_queue} --exec  \"${SCRIPT_DIR}/manager.sh\""
+flock -n ${consumer_lock} -c "ntfy subscribe ${ntfy_queue} \"${SCRIPT_DIR}/manager.sh\""
 
 exit 0
