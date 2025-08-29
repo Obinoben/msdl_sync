194026b4ff501d5e11b52d2ccead438f20911546
Ben Souverbie <ben@quadrumane.com>
2025-08-29 14:24:16 -0400

    push log

--- scripts/manager.sh
+++ scripts/manager.sh
@@ -8,6 +8,7 @@ MONDAY_BOARD=$(curl -s -X POST "https://api.monday.com/v2" \
                    -H "Content-Type: application/json" \
                    -d "{\"query\": \"query { items(ids: $MONDAY_ITEM) { board { id } } }\"}" \
                    | jq -r '.data.items[0].board.id')
+LOG=""
 
 update_monday_item () {
   local board_id="$1"
@@ -26,20 +27,38 @@ update_monday_item () {
     -d "$QUERY_JSON"
 }
 
-echo "Message reçu"
+update_log () {
+  local message="$1"
+  local push=$2
+
+  echo $message
+  LOG="${LOG}${message}\n"
+
+  if [[ $push -eq 1 ]]; then
+    update_monday_item $MONDAY_BOARD $MONDAY_ITEM "Log" $message
+  fi
+}
+
+update_log "Message reçu" 1
 update_monday_item $MONDAY_BOARD $MONDAY_ITEM "status" "Traitement"
 global=0
 for archive in $(echo "$m" | jq -r '.archives' | sed 's/,//g'); do
-  echo "Lance la synchronisation de ${archive}"
+  update_log "Lance la synchronisation de ${archive}" 1
   ${SCRIPT_DIR}/syncer.py -s -b ${archive} --force
-  global=$(( $global + $? ))
+  status=$?
+  global=$(( $global + $status ))
+  if [[ $status -eq 0 ]]; then
+    update_log ">> OK" 1
+  else
+    update_log ">> Échec" 1
+  fi
 done
 
 if [[ $global -eq 0 ]]; then
-  echo "Tout a fonctionné"
+  update_log "Tout a fonctionné" 1
   update_monday_item $MONDAY_BOARD $MONDAY_ITEM "status" "Succès"
 else
-  echo "Erreur de sync"
+  update_log "Erreur de sync" 1
   update_monday_item $MONDAY_BOARD $MONDAY_ITEM "status" "Échec"
 fi
 
